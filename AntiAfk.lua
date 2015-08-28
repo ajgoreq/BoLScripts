7f77bf2ff8964d76711211de5adc28d1fb4c52dc
ScoopyBoL <szymonpuchala123@o2.pl>
2015-08-28 20:48:14 +0200

    Update AntiAfk.lua

--- AntiAfk.lua
+++ AntiAfk.lua
@@ -10,12 +10,14 @@ AntiAFKSystem()
 end
 
 function Menu()
+  Clock = os.clock()
 Config = scriptConfig("Anti AFK By Scoopy", "antiafksc")
 Config:addParam("antisc", "Anti AFK On/Off", SCRIPT_PARAM_ONKEYTOGGLE, false, string.byte("N"))
 end
 
 function AntiAFKSystem()
-  if Config.antisc then
+   if os.clock() < Clock or not Config.antisc then return end
+    Clock = os.clock() + math.random(60,120)
 myHero:MoveTo(myHero.x, myHero.z)
   end
 end
